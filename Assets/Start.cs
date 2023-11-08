5cdf263174b2c5802752ba8d59bb7e71ac0fed4f
gfhj0001 <gfhj0001@naver.com>
2023-11-08 21:56:14 +0900

    2023/11/08 연습

---
+++ Assets/Start.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // 처음 시작했을 때 실행됨.
+    void Start()
+    {
+        
+    }
+
+    // 설정한 만큼 프레임에 들어갈 때 마다 실행됨.
+    void Update()
+    {
+        
+    }
+}
