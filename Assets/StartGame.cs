9f1ebb27a2bd0a4ed1401b1da45b71cea5333040
jinjin-0 <jhlee00987@gmail.com>
2024-03-27 16:57:19 +0900

    처음 실행 화면부터 4가지의 메뉴 씬 전환이동
    게임 실행 후 메인화면에서 게임 시작누르면 practice 씬으로 전환하여 게임 진행가능

--- Assets/StartGame.cs
+++ Assets/StartGame.cs
@@ -6,23 +6,8 @@ using UnityEngine.SceneManagement;
 public class StartGame : MonoBehaviour
 {
 
-    public void StartBtn(){
-        SceneManager.LoadScene("Main");
+    public void StartBtn()
+    {
+        SceneManager.LoadScene("practice");
     }
-
-
-
-
-
-    // // 처음 시작했을 때 실행됨.
-    // void Start()
-    // {
-        
-    // }
-
-    // // 설정한 만큼 프레임에 들어갈 때 마다 실행됨.
-    // void Update()
-    // {
-        
-    // }
 }
--- Assets/Undead Survivor/Code/Game Manager.cs
+++ Assets/Undead Survivor/Code/Game Manager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
 
     public float gameTime;
-    public float maxGameTime = 2 * 10f;
+    public float maxGameTime = 2 * 1000f;
 
 
     public PoolManager Pool;
