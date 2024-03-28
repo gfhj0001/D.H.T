d3a86ad190c9186a12aee553c1cbc95d89bf0c4f
miliys <miliys2004@naver.com>
2024-03-28 21:12:55 +0900

    Revert "처음 실행 화면부터 4가지의 메뉴 씬 전환이동"
    This reverts commit 9f1ebb27a2bd0a4ed1401b1da45b71cea5333040.

--- Assets/StartGame.cs
+++ Assets/StartGame.cs
@@ -6,8 +6,23 @@ using UnityEngine.SceneManagement;
 public class StartGame : MonoBehaviour
 {
 
-    public void StartBtn()
-    {
-        SceneManager.LoadScene("practice");
+    public void StartBtn(){
+        SceneManager.LoadScene("Main");
     }
+
+
+
+
+
+    // // 처음 시작했을 때 실행됨.
+    // void Start()
+    // {
+        
+    // }
+
+    // // 설정한 만큼 프레임에 들어갈 때 마다 실행됨.
+    // void Update()
+    // {
+        
+    // }
 }
