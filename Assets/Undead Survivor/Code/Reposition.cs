9af198d1e66d3ccff52b8bfe2d9ccbaf17b3080a
miliys <miliys2004@naver.com>
2024-04-09 00:24:13 +0900

    무한맵 버그 다시 수정
     diffX와 diffY가 같을 경우의 경우에 switch문에 else를 넣어 diffX == diffY 경우 같이 이동하게 설정하여 오류 해결

--- Assets/Undead Survivor/Code/Reposition.cs
+++ Assets/Undead Survivor/Code/Reposition.cs
@@ -2,48 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Repostion : MonoBehaviour
+public class Reposition : MonoBehaviour
 {
-    Collider2D coll;
-
-    void Awake()
-    {
-        coll = GetComponent<Collider2D>();
-    }
-    
-    private void OnTriggerExit2D(Collider2D collision)
+    void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Area"))
             return;
 
-        Vector3 Playerpos = GameManager.instance.player.transform.position;
+        Vector3 playerPos = GameManager.instance.player.transform.position;
         Vector3 myPos = transform.position;
-        float diffX = Mathf.Abs(Playerpos.x - myPos.x);
-        float diffY = Mathf.Abs(Playerpos.y - myPos.y);
+        float diffX = Mathf.Abs(playerPos.x - myPos.x);
+        float diffY = Mathf.Abs(playerPos.y - myPos.y);
 
-        Vector3 platerDir = GameManager.instance.player.inputVec;
-        float dirX = platerDir.x < 0 ? -1 : 1;
-        float dirY = platerDir.y < 0 ? -1 : 1;
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        float dirX = playerDir.x < 0 ? -1 : 1;
+        float dirY = playerDir.y < 0 ? -1 : 1;
 
-        switch (transform.tag)
-        {
+        switch (transform.tag) {
             case "Ground":
-                // 타일맵 하나의 크기는 20 -> 두 칸 건너뛰어야 하니 40
-                if (diffX > diffY) {
-                    transform.Translate(dirX * 40, 0, 0);
-                } else if (diffX < diffY) {
-                    transform.Translate(0, dirY * 40, 0);
-                } else {
-                    transform.Translate(dirX * 40, dirY * 40, 0);
+                if (diffX > diffY)
+                {
+                transform.Translate(Vector3.right * dirX * 40); 
+                }
+                else if (diffX < diffY)
+                {
+                transform.Translate(Vector3.up * dirY * 40);
+                }
+                else
+                {
+                transform.Translate(Vector3.right * dirX * 40);
+                transform.Translate(Vector3.up * dirY * 40);
                 }
                 break;
             case "Enemy":
-                if (coll.enabled)
-                {
-                    transform.Translate(platerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f));
-                }
                 break;
         }
-
     }
 }
